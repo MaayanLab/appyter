a0a7207582449516218f0d44c57f16b49b2877ca
Daniel J. B. Clarke <u8sand@gmail.com>
2020-03-11 12:43:43 -0400

    Add json renderer

---
+++ jupyter_template/render/json.py
@@ -0,0 +1,10 @@
+import json
+from jupyter_template.parse.nbtemplate import parse_fields_from_nbtemplate
+
+def render_json_from_nbtemplate(env, nb):
+  ''' Render a json representing the relevant Fields throughout the notebook.
+  '''
+  return json.dumps([
+    field.args
+    for field in parse_fields_from_nbtemplate(env, nb)
+  ])
