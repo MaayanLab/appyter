50a7d3010410ebc31d900f5c02d75ff596c727f2
Daniel J. B. Clarke <u8sand@gmail.com>
2020-03-13 10:07:45 -0400

    Recognize on/off checked/unchecked for boolfield

--- jupyter_template/profiles/default/fields/BoolField.py
+++ jupyter_template/profiles/default/fields/BoolField.py
@@ -4,9 +4,9 @@ from jupyter_template.fields import Field
 class BoolField(Field):
   @property
   def raw_value(self):
-    if self.args['value'] in {'true', 'True', True}:
+    if self.args['value'] in {'on', 'checked', 'true', 'True', True}:
       return True
-    elif self.args['value'] in {'false', 'False', False}:
+    elif self.args['value'] in {'off', 'unchecked', 'false', 'False', False}:
       return False
     else:
       raise Exception('Invalid value for bool field')
