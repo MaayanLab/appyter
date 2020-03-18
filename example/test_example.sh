74d959d82d65e8a8688323138fd1f316e9c8e791
Daniel J. B. Clarke <u8sand@gmail.com>
2020-03-18 16:42:58 -0400

    Demonstrate biojupies profile in example

--- example/test_example.sh
+++ example/test_example.sh
@@ -5,6 +5,6 @@ echo "Try it out at http://0.0.0.0:5000/"
 echo "-----------------------------------------"
 echo ""
 cd .. && HOST=0.0.0.0 PORT=5000 python3 -m jupyter_template \
-  --profile=bootstrap \
+  --profile=biojupies \
   --cwd=example \
   example/example.ipynb
