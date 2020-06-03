78f10d555fd9d72e20f126049fab5041ead50c4f
Daniel Clarke <u8sand@gmail.com>
2020-06-03 10:11:19 -0400

    Update example to include a companion script

---
+++ example/test.py
@@ -0,0 +1,2 @@
+def my_test_func():
+  print('hello world')
--- example/test_example.sh
+++ example/test_example.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 echo "-----------------------------------------"
-echo "Try it out at http://0.0.0.0:5000/"
+echo "Try it out at http://0.0.0.0:5000/example/"
 echo "-----------------------------------------"
 echo ""
 cd .. && HOST=0.0.0.0 PORT=5000 PREFIX=/example/ python3 -m appyter \
