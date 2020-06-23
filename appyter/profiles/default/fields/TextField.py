5978e3338f173a7c4544205085db2eb96d3f8dcd
Daniel Clarke <u8sand@gmail.com>
2020-06-23 17:55:37 -0400

    Passthrough rows and cols to textarea

--- appyter/profiles/default/fields/TextField.py
+++ appyter/profiles/default/fields/TextField.py
@@ -9,9 +9,11 @@ class TextField(Field):
   :param name: (str) A name that will be used to refer to the object as a variable and in the HTML form.
   :param label: (str) A human readable label for the field for the HTML form
   :param description: (Optional[str]) A long human readable description for the field for the HTML form
-  :param constraint: A regular expression for validating the file name.
-  :param hint: A hint to put in the field prior to content.
+  :param constraint: (Regex[str]) A regular expression for validating the file name.
   :param default: (str) A default value as an example and for use during prototyping
+  :param hint: (Optional[str]) A hint to put in the field prior to content.
+  :param rows: (Optional[int]) The number of rows (lines) in the textarea
+  :param cols: (Optional[int]) The number of cols (horizontal characters) in the textarea
   :param section: (Optional[str]) The name of a SectionField for which to nest this field under, defaults to a root SectionField
   :param value: (INTERNAL Any) The raw value of the field (from the form for instance)
   :param \**kwargs: Remaining arguments passed down to :class:`appyter.fields.Field`'s constructor.
--- appyter/profiles/default/fields/TextListField.py
+++ appyter/profiles/default/fields/TextListField.py
@@ -14,9 +14,11 @@ class TextListField(Field):
   :param label: (str) A human readable label for the field for the HTML form
   :param description: (Optional[str]) A long human readable description for the field for the HTML form
   :param choices: (Union[List[str], Set[str], Dict[str, str]]) A set of choices that are available for this field or lookup table mapping from choice label to resulting value
-  :param constraint: A regular expression for validating the file name.
-  :param hint: A hint to put in the field prior to content.
+  :param constraint: (Regex[str]) A regular expression for validating the file name.
   :param default: (str) A default value as an example and for use during prototyping
+  :param hint: (Optional[str]) A hint to put in the field prior to content.
+  :param rows: (Optional[int]) The number of rows (lines) in the textarea
+  :param cols: (Optional[int]) The number of cols (horizontal characters) in the textarea
   :param section: (Optional[str]) The name of a SectionField for which to nest this field under, defaults to a root SectionField
   :param value: (INTERNAL Any) The raw value of the field (from the form for instance)
   :param \**kwargs: Remaining arguments passed down to :class:`appyter.fields.Field`'s constructor.
