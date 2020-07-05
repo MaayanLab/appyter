e78fa7b2fe60938c47a9308ee3a62354de8cc216
Daniel Clarke <u8sand@gmail.com>
2020-07-05 18:14:28 -0400

    Migrate to nbclient and fix carriage returns
    Fixes #36
    Fixes #35

---
+++ appyter/ext/nbclient/__init__.py
@@ -0,0 +1,29 @@
+from nbclient import NotebookClient
+from nbclient.util import ensure_async
+from nbclient.exceptions import CellExecutionComplete
+
+class NotebookClientIOPubHook(NotebookClient):
+  ''' A notebook client with the ability to hook into iopub updates
+  '''
+  def __init__(self, *args, iopub_hook=None, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.iopub_hook = iopub_hook
+
+  async def _async_poll_output_msg(
+    self,
+    parent_msg_id,
+    cell,
+    cell_index
+  ):
+    assert self.kc is not None
+    while True:
+      msg = await ensure_async(self.kc.iopub_channel.get_msg(timeout=None))
+      if msg['parent_header'].get('msg_id') == parent_msg_id:
+        try:
+          # Will raise CellExecutionComplete when completed
+          self.process_message(msg, cell, cell_index)
+        except CellExecutionComplete:
+          return
+        # Process iopub_hook
+        if self.iopub_hook is not None:
+          self.iopub_hook(cell, cell_index)
--- appyter/render/nbexecutor/__init__.py
+++ appyter/render/nbexecutor/__init__.py
@@ -1,6 +1,9 @@
-import os, sys
+import os
+import sys
+import functools
+
+from appyter.ext.nbclient import NotebookClientIOPubHook
 from appyter.render.nbviewer import render_nbviewer_from_nb
-from appyter.render.nbexecutor.yielding_preprocessor import YieldingExecutePreprocessor
 
 def cell_is_code(cell):
     return cell.get('cell_type') == 'code'
@@ -11,12 +14,18 @@ def cell_has_error(cell):
             return True
     return False
 
+def iopub_hook_factory(nb, emit):
+  def iopub_hook(cell, cell_index):
+    emit('cell', [cell, cell_index])
+  return iopub_hook
+
 def render_nbexecutor_from_nb(env, nb):
   def nbexecutor(emit=print, session_dir='', cleanup=None, stopper=lambda: False):
     assert callable(emit), 'Emit must be callable'
     try:
       emit('status', 'Starting')
-      yep = YieldingExecutePreprocessor(
+      client = NotebookClientIOPubHook(
+        nb,
         allow_errors=True,
         timeout=None,
         kernel_name='python3',
@@ -24,26 +33,27 @@ def render_nbexecutor_from_nb(env, nb):
           PYTHONPATH=':'.join(sys.path),
           PATH=os.environ['PATH'],
         ),
+        resources={ 'metadata': {'path': session_dir} },
+        iopub_hook=iopub_hook_factory(nb, emit),
       )
-      resources = {'metadata': {'path': session_dir} }
-      index = 0
-      code_cell_index = 0
-      n_cells = len(nb.cells)
-      emit('status', 'Executing...')
-      emit('progress', index)
-      for cell, _ in yep.preprocess(nb, resources):
-        assert not stopper(), "Stopper"
-        emit('cell', cell)
-        if cell_is_code(cell):
-          code_cell_index += 1
-          if cell_has_error(cell):
-            raise Exception('Cell execution error on cell %d' % (code_cell_index))
-        index += 1
-        if index < n_cells:
-          emit('progress', index)
-        else:
-          emit('status', 'Success')
-        assert not stopper(), "Stopper"
+      with client.setup_kernel():
+        n_cells = len(nb.cells)
+        emit('status', 'Executing...')
+        emit('progress', 0)
+        for index, cell in enumerate(nb.cells):
+          assert not stopper(), "Stopper"
+          cell = client.execute_cell(
+            cell, index,
+            execution_count=client.code_cells_executed + 1,
+          )
+          if cell_is_code(cell):
+            if cell_has_error(cell):
+              raise Exception('Cell execution error on cell %d' % (index))
+          if index < n_cells-1:
+            emit('progress', index+1)
+          else:
+            emit('status', 'Success')
+          assert not stopper(), "Stopper"
     except Exception as e:
       print('error', e)
       emit('error', str(e))
