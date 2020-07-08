5189d3fe0b678cd47cffafc66c15ae2c848f563d
Daniel Clarke <u8sand@gmail.com>
2020-07-08 18:13:28 -0400

    Fix requirements.txt git ref notation

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup, find_packages
 
-requirements = list(map(str.strip, open('requirements.txt', 'r').readlines()))
-
 setup(
   name='appyter',
-  version='0.6.6',
+  version='0.6.7',
   url='https://github.com/maayanLab/appyter/',
   author='Daniel J. B. Clarke',
   author_email='u8sand@gmail.com',
   long_description=open('README.md', 'r').read(),
   license='Apache-2.0',
-  install_requires=[requirement for requirement in requirements if '://' not in requirement],
-  dependency_links=[requirement for requirement in requirements if '://' in requirement],
+  install_requires=list(map(str.strip, open('requirements.txt', 'r').readlines())),
   packages=find_packages(exclude=('example',)),
   include_package_data=True,
   entry_points={
