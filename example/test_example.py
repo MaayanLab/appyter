9fb992c1ebd08b20196c5047c8f0d64434b6641a
Daniel Clarke <u8sand@gmail.com>
2020-07-15 14:49:21 -0400

    Fixed test_example.py with get_env

--- example/test_example.py
+++ example/test_example.py
@@ -3,19 +3,24 @@ import os, sys; sys.path.insert(0, os.path.realpath('..'))
 from appyter.parse.nb import nb_from_ipynb_file
 from appyter.render.nbconstruct import render_nb_from_nbtemplate
 from appyter.render.form import render_form_from_nbtemplate
-from appyter.context import get_jinja2_env
+from appyter.context import get_jinja2_env, get_env
 from appyter.parse.nbtemplate import parse_fields_from_nbtemplate
+from appyter.parse.nb import parse_markdown
 
 nbtemplate = nb_from_ipynb_file('example.ipynb')
 
-env = get_jinja2_env()
+config = get_env(cwd=os.path.dirname(__file__), ipynb='example.ipynb')
+env = get_jinja2_env(config=config)
 parse_fields_from_nbtemplate(env, nbtemplate)
 
-env = get_jinja2_env()
+env = get_jinja2_env(config=config)
 render_form_from_nbtemplate(env, nbtemplate)
 
-env = get_jinja2_env({
-  'number_1': 8,
-  'operator': 'subtract',
-})
+env = get_jinja2_env(
+  context={
+    'number_1': 8,
+    'operator': 'subtract',
+  },
+  config=config,
+)
 render_nb_from_nbtemplate(env, nbtemplate)
