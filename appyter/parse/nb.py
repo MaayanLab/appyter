f1e81831bdadfb4b175f0eba8064a6c54506b845
Daniel Clarke <u8sand@gmail.com>
2020-07-23 17:10:15 -0400

    Add the ability to go to/from nb/json

--- appyter/parse/nb.py
+++ appyter/parse/nb.py
@@ -1,3 +1,4 @@
+import json
 import nbformat as nbf
 
 def nb_from_ipynb_string(string):
@@ -6,6 +7,9 @@ def nb_from_ipynb_string(string):
 def nb_from_ipynb_file(filename):
   with open(filename, 'r') as fr:
     return nbf.read(fr, as_version=4)
+
+def nb_from_json(j):
+  return nb_from_ipynb_string(json.dumps(j))
 
 def nb_to_ipynb_string(nb):
   return nbf.writes(nb)
@@ -16,3 +20,6 @@ def nb_to_ipynb_io(nb, io):
 def nb_to_ipynb_file(nb, filename):
   with open(filename, 'w') as fw:
     nb_to_ipynb_io(nb, fw)
+
+def nb_to_json(nb):
+  return json.loads(nb_to_ipynb_string(nb))
