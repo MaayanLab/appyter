fc1cb0c2bed65b5294c0cadc0b9d886b1d1e6a47
Daniel Clarke <u8sand@gmail.com>
2020-08-07 17:41:13 -0400

    Publish new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='appyter',
-  version='0.7.8',
+  version='0.7.9',
   url='https://github.com/maayanLab/appyter/',
   author='Daniel J. B. Clarke',
   author_email='u8sand@gmail.com',
