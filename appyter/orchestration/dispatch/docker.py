a09464f99c59dced5a447785dbafed64d1e0bd7e
Daniel Clarke <u8sand@gmail.com>
2020-08-26 19:10:52 -0400

    Eliminate interactive tty for docker dispatch

--- appyter/orchestration/dispatch/docker.py
+++ appyter/orchestration/dispatch/docker.py
@@ -5,8 +5,7 @@ import json
 
 def dispatch(job=None, Popen=None, **kwargs):
   with Popen([
-    'docker', 'run',
-    '-it', job['image'],
+    'docker', 'run', job['image'],
     'appyter', 'orchestration', 'job', json.dumps(job)
   ]) as proc:
     proc.wait()
