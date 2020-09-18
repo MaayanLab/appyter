862e55017854e7dbd2eb83498f5d56a6dd50da55
Daniel Clarke <u8sand@gmail.com>
2020-09-18 12:00:30 -0400

    Replace jq with pyeval
    Hopefully helps support for windows

--- appyter/profiles/default/filters/jq.py
+++
@@ -1,8 +0,0 @@
-import json
-from jq import jq as JQ
-
-def jq(obj, id_query, query):
-  obj_lookup = dict(zip(JQ(id_query).transform(obj, multiple_output=True), obj))
-  results = JQ(query).transform(obj, multiple_output=True)
-  result_ids = JQ(id_query).transform(results, multiple_output=True)
-  return [obj_lookup[id] for id, result in zip(result_ids, results)]
