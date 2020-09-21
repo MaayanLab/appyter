f3a3d3df0f5abc18b9c7979041091a02a67103d3
Daniel Clarke <u8sand@gmail.com>
2020-09-21 19:01:22 -0400

    Run docker dispatch in the same network as current image

--- appyter/orchestration/dispatch/docker.py
+++ appyter/orchestration/dispatch/docker.py
@@ -1,11 +1,24 @@
 ''' Launch job using docker directly
 '''
 
+import os
 import json
+from subprocess import PIPE
 
 def dispatch(job=None, Popen=None, **kwargs):
-  with Popen([
-    'docker', 'run', job['image'],
-    'appyter', 'orchestration', 'job', json.dumps(job)
-  ]) as proc:
+  args = ['docker', 'run']
+  try:
+    with Popen([
+      'docker', 'inspect', os.environ['HOSTNAME'],
+    ], stdout=PIPE) as proc:
+      conf = json.load(proc.stdout)
+    args += ['--network', conf[0]['HostConfig']['NetworkMode']]
+  except:
+    args += []
+  #
+  args += [
+    job['image'],
+    'appyter', 'orchestration', 'job', json.dumps(job),
+  ]
+  with Popen(args) as proc:
     proc.wait()
