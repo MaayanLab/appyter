933fe99ce77dd2e82029c2060b29f8e439d011e7
Daniel Clarke <u8sand@gmail.com>
2020-09-25 15:23:50 -0400

    Fix to nbviewer render

--- appyter/render/nbviewer.py
+++ appyter/render/nbviewer.py
@@ -1,6 +1,11 @@
 import nbconvert
 from bs4 import BeautifulSoup
 
+def find_and_decompose(soup, obj):
+  el = soup.find(obj)
+  if el:
+    el.decompose()
+
 def render_nbviewer_from_nb(env, nb):
   ''' Render an nbviewer (HTML serialization of the notebook)
   '''
@@ -8,13 +13,13 @@ def render_nbviewer_from_nb(env, nb):
   export, _ = exporter.from_notebook_node(nb)
   # Strip things from export
   soup = BeautifulSoup(export, 'html.parser')
-  soup.find('meta').decompose()  # remove meta
-  soup.find('title').decompose()  # remove title
+  find_and_decompose(soup, 'meta')  # remove meta
+  find_and_decompose(soup, 'title')  # remove title
   # remove first 2 scripts (require.js and jquery)
-  soup.find('script').decompose()
-  soup.find('script').decompose()
-  # soup.find('style').decompose() # remove first style (bootstrap)
-  soup.find('link').decompose()  # remove link to custom stylesheet
+  find_and_decompose(soup, 'script')
+  find_and_decompose(soup, 'script')
+  # find_and_decompose(soup, 'style') # remove first style (bootstrap)
+  find_and_decompose(soup, 'link')  # remove link to custom stylesheet
   nb_container = soup.select('#notebook-container')[0]
   nb_container['class'] = ''
   nb_container['id'] = ''
