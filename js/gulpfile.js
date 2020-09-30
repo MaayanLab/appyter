d5ed81a62c8a02052ada1d1ddb6eec9368026dd4
Daniel Clarke <u8sand@gmail.com>
2020-09-30 12:13:10 -0400

    Updated gulpfile adding support for building sass & js + tinify/clean-css

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -3,21 +3,53 @@ const gulp = require('gulp')
 const log = require('gulplog')
 const tap = require('gulp-tap')
 const buffer = require('gulp-buffer')
-const sourcemaps = require('gulp-sourcemaps')
 const rename = require('gulp-rename')
+const cleanCSS = require('gulp-clean-css')
 const browserify = require('browserify')
 const sveltify = require('sveltify')
 const babelify = require('babelify')
+const tinyify = require('tinyify')
+const sass = require('gulp-sass')
 const root = path.join(__dirname, '..')
 
-const js = {
+const js_files = {
+  src: path.join(root, 'js/profiles/**/*.js'),
+  dest: path.join(root, 'appyter', 'profiles'),
+}
+
+const svelte_files = {
   src: path.join(root, 'js/profiles/**/*.svelte'),
   dest: path.join(root, 'appyter', 'profiles'),
 }
 
-gulp.task('build', function () {
+const css_files = {
+  src: path.join(root, 'js/profiles/**/*.scss'),
+  dest: path.join(root, 'appyter', 'profiles'),
+}
+
+gulp.task('build_js', function () {
   return gulp
-    .src(js.src, { read: false, since: gulp.lastRun('build') })
+    .src(js_files.src, { read: false, since: gulp.lastRun('build_js') })
+    .pipe(tap(function (file) {
+      log.info('bundling ' + file.path)
+      file.contents = browserify(file.path, {
+        debug: true,
+        standalone: file.basename,
+        transform: [
+          [babelify, { presets: ['@babel/preset-env'] }],
+        ],
+        plugin: [
+          tinyify,
+        ],
+      }).bundle()
+    }))
+    .pipe(buffer())
+    .pipe(gulp.dest(js_files.dest))
+})
+
+gulp.task('build_svelte', function () {
+  return gulp
+    .src(svelte_files.src, { read: false, since: gulp.lastRun('build_svelte') })
     .pipe(tap(function (file) {
       log.info('bundling ' + file.path)
       file.contents = browserify(file.path, {
@@ -27,18 +59,39 @@ gulp.task('build', function () {
           [babelify, {presets: ['@babel/preset-env']}],
           sveltify,
         ],
+        plugin: [
+          tinyify,
+        ],
       }).bundle()
     }))
     .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(rename(function (path) {
       path.extname = ".js"
     }))
-    .pipe(gulp.dest(js.dest))
+    .pipe(gulp.dest(svelte_files.dest))
 })
 
+gulp.task('build_css', function () {
+  return gulp
+    .src(css_files.src, { since: gulp.lastRun('build_css') })
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(rename(function (path) {
+      path.extname = ".css"
+    }))
+    .pipe(gulp.dest(css_files.dest))
+})
+
+gulp.task('build', gulp.series([
+  'build_js',
+  'build_svelte', 
+  'build_css',
+]))
+
 gulp.task('watch', function () {
-  return gulp.watch(js.src, gulp.task('build'))
+  gulp.watch(js_files.src, gulp.task('build_js'))
+  gulp.watch(svelte_files.src, gulp.task('build_svelte'))
+  gulp.watch(css_files.src, gulp.task('build_css'))
 })
 
 gulp.task('default', gulp.series(['build', 'watch']))
