be259b8728242104c194e158c0076a70e8fb69cb
Daniel Clarke <u8sand@gmail.com>
2020-10-02 12:24:12 -0400

    NotebookClientIOPubHook refactor

--- appyter/ext/nbclient/__init__.py
+++ appyter/ext/nbclient/__init__.py
@@ -8,7 +8,6 @@ class NotebookClientIOPubHook(NotebookClient):
   def __init__(self, *args, iopub_hook=None, **kwargs):
     super().__init__(*args, **kwargs)
     self.iopub_hook = iopub_hook
-    self.kc = None
 
   async def _async_poll_output_msg(
     self,
@@ -17,14 +16,15 @@ class NotebookClientIOPubHook(NotebookClient):
     cell_index
   ):
     assert self.kc is not None
-    while True:
+    complete = False
+    while not complete:
       msg = await ensure_async(self.kc.iopub_channel.get_msg(timeout=None))
       if msg['parent_header'].get('msg_id') == parent_msg_id:
         try:
           # Will raise CellExecutionComplete when completed
           self.process_message(msg, cell, cell_index)
         except CellExecutionComplete:
-          return
-        # Process iopub_hook
-        if self.iopub_hook is not None:
-          await self.iopub_hook(cell, cell_index)
+          complete = True
+        finally:
+          if self.iopub_hook is not None:
+            await self.iopub_hook(cell, cell_index)
