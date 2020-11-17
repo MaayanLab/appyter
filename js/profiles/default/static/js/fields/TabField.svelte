1c1002c5e730a9ad7a2912e77173591c3d0d4b72
Daniel Clarke <u8sand@gmail.com>
2020-11-17 15:25:35 -0500

    Eliminate unnecessary ml-1 on icon

--- js/profiles/default/static/js/fields/TabField.svelte
+++ js/profiles/default/static/js/fields/TabField.svelte
@@ -7,7 +7,7 @@
   <div class="col-lg-2 bold text-lg-right my-auto">
     {args.label}
     {#if args.description}
-      <sup><i class="far fa-question-circle ml-1" data-toggle="tooltip" title="" data-original-title="{args.description}"></i></sup>
+      <sup data-toggle="tooltip" title={args.description}><i class="far fa-question-circle"></i></sup>
     {/if}:
   </div>
   <div class="col-lg-2 pt-2 pt-lg-0">
