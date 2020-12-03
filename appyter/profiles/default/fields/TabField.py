77c2b07c20d7f0d2c697d27ce0d80c574c1b3418
Daniel Clarke <u8sand@gmail.com>
2020-12-03 12:26:10 -0500

    TabField should also passthrough prepare/pre/post construct

--- appyter/profiles/default/fields/TabField.py
+++ appyter/profiles/default/fields/TabField.py
@@ -1,4 +1,8 @@
+import logging
+logger = logging.getLogger(__name__)
+
 from appyter.fields import Field
+from appyter.util import collapse, try_json_loads
 
 class TabField(Field):
   '''
@@ -24,6 +28,30 @@ class TabField(Field):
   '''
   def __init__(self, choices={}, **kwargs):
     super(TabField, self).__init__(choices=choices, **kwargs)
+
+  def prepare(self, req):
+    logger.debug(f"prepare")
+    data = {}
+    if type(req) == dict:
+      data[self.args['name']] = self.args['value'] = req.get(self.args['name'])
+    elif req.json:
+      data[self.args['name']] = self.args['value'] = req.json.get(self.args['name'])
+    elif req.form:
+      data[self.args['name']] = self.args['value'] = collapse(req.form.getlist(self.args['name']))
+    else:
+      raise NotImplementedError
+    data.update(self.value.prepare(req))
+    return data
+
+  def pre_construct(self, ctx, **kwargs):
+    logger.debug(f"pre_construct")
+    self.args['value'] = ctx.get(self.args['name'])
+    return self.value.pre_construct(ctx, **kwargs)
+
+  def post_construct(self, ctx, **kwargs):
+    logger.debug(f"post_construct")
+    self.args['value'] = ctx.get(self.args['name'])
+    return self.value.post_construct(ctx, **kwargs)
 
   def constraint(self):
     return self.raw_value is not None and all(v in self.choices for v in self.raw_value)
