a240cf7598811401d386c949eaaf5a2a44347d9e
Daniel Clarke <u8sand@gmail.com>
2020-12-04 10:45:14 -0500

    Update python module license

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author='Daniel J. B. Clarke',
   author_email='u8sand@gmail.com',
   long_description=open('README.md', 'r').read(),
-  license='Apache-2.0',
+  license='CC-BY-NC-SA-4.0',
   install_requires=list(map(str.strip, open('requirements.txt', 'r').readlines())),
   extras_require={
     'production': list(map(str.strip, open('requirements.production.txt', 'r').readlines())),
