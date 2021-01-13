8c28a1bebce76ea4bb93b0067e0c8d879ff80951
Daniel Clarke <u8sand@gmail.com>
2021-01-13 14:23:42 -0500

    Improve DescriptionField docstring

--- appyter/profiles/default/fields/DescriptionField.py
+++ appyter/profiles/default/fields/DescriptionField.py
@@ -3,8 +3,17 @@ from appyter.fields import Field
 class DescriptionField(Field):
   ''' Representing text between fields
 
+  Usage:
+  ```python
+  {% do DescriptionField(
+    name='my_fieldname',
+    text='My Description',
+  ) %}
+  ```
+
   :param name: (str) A name that will be used to refer to the object as a variable and in the HTML form.
   :param text: (str) A human readable, HTML parsable text
+  :param section: (Optional[str]) The name of a SectionField for which to nest this field under, defaults to a root SectionField
   :param \**kwargs: Additional keyword arguments used by other fields
   '''
   def __init__(self, **kwargs):
