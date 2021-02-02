d8df76cb704a113311efe220b8036c100057c2b6
Daniel Clarke <u8sand@gmail.com>
2021-02-02 15:47:12 -0500

    Fixes for pypi release References #83
    - Add setup.py classifiers
    - Ignore some additional files created during pypi deployment
    - Add long_description_content_type to setup.py
    - Fix MANIFEST.in for pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,17 @@ setup(
   author='Daniel J. B. Clarke',
   author_email='u8sand@gmail.com',
   long_description=open('README.md', 'r').read(),
+  long_description_content_type='text/markdown; charset=UTF-8',
   license='CC-BY-NC-SA-4.0',
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Software Development :: Libraries :: Application Frameworks",
+    "Framework :: Jupyter",
+  ],
   install_requires=list(map(str.strip, open('requirements.txt', 'r').readlines())),
   extras_require={
     'production': list(map(str.strip, open('requirements.production.txt', 'r').readlines())),
