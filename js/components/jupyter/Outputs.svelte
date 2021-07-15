b7867e65ea306d6abaeb14049cdd674bf85cfcda
Daniel Clarke <u8sand@gmail.com>
2021-07-15 10:49:43 -0400

    Use index instead of hash for outputs
    References #100

--- js/components/jupyter/Outputs.svelte
+++ js/components/jupyter/Outputs.svelte
@@ -3,32 +3,32 @@
   import Loader from '@/components/Loader.svelte'
   import Prompt from '@/components/jupyter/Prompt.svelte'
   import collapse from '@/utils/collapse.js'
-  import hash from '@/utils/hash.js'
 
   export let data = []
   export let index
   export let loading
 
   function *reduce_output_streams(outputs) {
+    let i = 0
     let streams = {}
     for (const output of outputs) {
       if (output.output_type === 'stream') {
         if (streams[output.name] === undefined) {
           const output_text = collapse(output.text, '\n')
-          streams[output.name] = {...output, text: output_text}
+          streams[output.name] = {...output, text: output_text, index: i++}
         } else {
           const output_text = collapse(output.text, '\n')
           if (output_text.startsWith('\r')) streams[output.name].text = output_text
           else streams[output.name].text += output_text
         }
       } else {
-        yield {...output, hash: hash(output)}
+        yield {...output, index: i++}
       }
     }
     for (const stream in streams) {
       streams[stream].text = streams[stream].text.replace(/^\r/, '').replace(/(\r?\n)+$/, '')
       if (streams[stream].text) {
-        yield {...streams[stream], hash: hash(streams[stream])}
+        yield {...streams[stream], index: i++}
       }
     }
   }
@@ -47,10 +47,10 @@
         </div>
       </div>
     {/if}
-    {#each [...reduce_output_streams(data)] as output, output_index (output.hash)}
+    {#each [...reduce_output_streams(data)] as output (output.index)}
       <div class="output_area">
         <Prompt
-          index="{index}-{output_index}"
+          index="{index}-{output.index}"
           prompt_type="output"
         />
         <div class="output_subarea">
