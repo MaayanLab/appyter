867b0f6ba3d35932e49fe84e8b7790f520ae4bcc
Daniel Clarke <u8sand@gmail.com>
2021-11-17 16:12:48 -0500

    Some refactoring with pathlib

--- appyter/__main__.py
+++ appyter/__main__.py
@@ -5,9 +5,9 @@ def main():
   from dotenv import load_dotenv
   load_dotenv()
   # load everything permitting injection to cli
-  import os
+  from pathlib import Path
   from appyter.ext.importlib import importdir
-  importdir(os.path.join(os.path.dirname(__file__)), __package__, globals())
+  importdir(Path(__file__).parent, __package__, globals())
   # grab cli
   from appyter.cli import cli
   cli()
