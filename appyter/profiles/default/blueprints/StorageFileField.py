6068205e6069c70b5d5638a197652a917f7546ff
Daniel Clarke <u8sand@gmail.com>
2021-12-03 16:23:48 -0500

    WIP: Add StorageFileField blueprint for API driven fsspec

---
+++ appyter/profiles/default/blueprints/StorageFileField.py
@@ -0,0 +1,67 @@
+import re
+from flask import Blueprint, abort, jsonify, request
+from fsspec.core import url_to_fs
+from appyter.ext.fsspec.parse import parse_file_uri_qs
+from appyter.ext.flask import route_join_with_or_without_slash
+import logging
+logger = logging.getLogger(__name__)
+
+StorageFileField = Blueprint('StorageFileField', __name__)
+
+@route_join_with_or_without_slash(StorageFileField, '', methods=['GET'])
+def index():
+  abort(404)
+
+# TODO: worry about permissioned access to `storage://`
+# TODO: worry about access to `file://`
+# TODO: worry about access to other providers via API as a proxy
+
+@route_join_with_or_without_slash(StorageFileField, 'ls', methods=['GET'])
+@route_join_with_or_without_slash(StorageFileField, 'ls', '<path:path>', methods=['GET'])
+def ls(path=''):
+  url, qs = parse_file_uri_qs(path + '?' + request.query_string.decode())
+  fs, fs_path = url_to_fs(url, **qs)
+  try:
+    return jsonify(fs.ls(fs_path))
+  except Exception as e:
+    import traceback
+    traceback.print_exc()
+    abort(500)
+
+@route_join_with_or_without_slash(StorageFileField, 'info', methods=['GET'])
+@route_join_with_or_without_slash(StorageFileField, 'info', '<path:path>', methods=['GET'])
+def info(path=''):
+  url, qs = parse_file_uri_qs(path + '?' + request.query_string.decode())
+  fs, fs_path = url_to_fs(url, **qs)
+  try:
+    return jsonify(fs.info(fs_path))
+  except Exception as e:
+    import traceback
+    traceback.print_exc()
+    abort(500)
+
+@route_join_with_or_without_slash(StorageFileField, 'cat', methods=['GET'])
+@route_join_with_or_without_slash(StorageFileField, 'cat', '<path:path>', methods=['GET'])
+def cat(path=''):
+  url, qs = parse_file_uri_qs(path + '?' + request.query_string.decode())
+  fs, fs_path = url_to_fs(url, **qs)
+  try:
+    return fs.cat(fs_path)
+  except Exception as e:
+    import traceback
+    traceback.print_exc()
+    abort(500)
+
+@route_join_with_or_without_slash(StorageFileField, 'read_block', methods=['GET'])
+@route_join_with_or_without_slash(StorageFileField, 'read_block', '<path:path>', methods=['GET'])
+def read_block(path=''):
+  m = re.match(r'^(bytes )?(\d+)-(\d+)$', request.headers['Range'])
+  _, start, end = m.groups()
+  url, qs = parse_file_uri_qs(path + '?' + request.query_string.decode())
+  fs, fs_path = url_to_fs(url, **qs)
+  try:
+    return fs.read_block(fs_path, start, end - start)
+  except Exception as e:
+    import traceback
+    traceback.print_exc()
+    abort(500)
