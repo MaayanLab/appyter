2edc17c64289b68897075f80d565bd29ca697dbf
Daniel Clarke <u8sand@gmail.com>
2022-03-21 15:17:47 -0400

    [ext.hashlib]: generic hashsum_io/hashsum_dict methods

--- appyter/ext/hashlib.py
+++ appyter/ext/hashlib.py
@@ -1,15 +1,20 @@
+import functools
 
-def sha1sum_io(io, chunk_size=65536):
+def hashsum_io(algo, io, chunk_size=65536):
   import hashlib
-  sha1 = hashlib.sha1()
+  hashsum = hashlib.new(algo)
   while True:
     buf = io.read(chunk_size)
     if not buf: break
-    sha1.update(buf)
-  return sha1.hexdigest()
+    hashsum.update(buf)
+  return hashsum.hexdigest()
 
-def sha1sum_dict(obj):
+sha1sum_io = functools.partial(hashsum_io, 'sha1')
+
+def hashsum_dict(algo, obj):
   import json, hashlib
-  sha1 = hashlib.sha1()
+  sha1 = hashlib.new(algo)
   sha1.update(json.dumps(obj, sort_keys=True, separators=None, ensure_ascii=True).encode())
   return sha1.hexdigest()
+
+sha1sum_dict = functools.partial(hashsum_dict, 'sha1')
