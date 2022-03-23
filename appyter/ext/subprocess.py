7e932290bd687d30026766fa36073139c568c22c
Daniel Clarke <u8sand@gmail.com>
2022-03-23 10:36:29 -0400

    Handle interrupt of closed subprocess

--- appyter/ext/subprocess.py
+++ appyter/ext/subprocess.py
@@ -5,6 +5,8 @@ import multiprocessing as mp
 def interrupt(proc: mp.Process):
   try:
     os.kill(proc.pid, signal.SIGINT)
+  except ProcessLookupError:
+    pass
   except KeyboardInterrupt:
     raise
   except:
