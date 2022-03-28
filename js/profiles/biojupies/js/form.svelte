e4dc5c3d32a288425d05074f2b74213607a60635
Daniel Clarke <u8sand@gmail.com>
2022-03-28 17:52:47 -0400

    WIP: Add user launch settings

--- js/profiles/biojupies/js/form.svelte
+++ js/profiles/biojupies/js/form.svelte
@@ -75,6 +75,47 @@
       />
     {/if}
   {/each}
+  {#if window._config.EXTRAS.indexOf('catalog-integration') !== -1 && $auth.state === 'auth'}
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="mt-3 mb-0 w-100 border-custom rounded bg-lightgrey" data-toggle="collapse">
+          <div class="d-table w-100 py-2 px-2 py-md-0 px-md-0">
+            <div class="d-table-cell align-middle card-icon px-3 py-3 text-center">
+              <i class="fas fa-cogs fa-2x" />
+            </div>
+            <div class="d-table-cell align-middle">
+              <div class="very-small regular pb-2 pr-5">User Launch Settings</div>
+              <div class="tiny light text-muted">Configure how the appyter launches</div>
+            </div>
+          </div>
+        </div>
+        <div class="border-grey mx-2 border-top-0 rounded-bottom tiny collapse show">
+          <div class="pt-3">
+            <div class="row px-3 pb-3">
+              <div class="col-lg-3 bold text-lg-right my-auto">
+                Execution Profile:
+                <sup data-toggle="tooltip" title="How the appyter should execute"><i class="far fa-question-circle"></i></sup>
+              </div>
+              <div class="col-lg-6 pt-2 pt-lg-0">
+                <select
+                  name="_executor"
+                  class="form-control nodecoration tiny bg-white px-2 py-1 mb-0"
+                >
+                  <option selected value="default">Execute in appyter catalog</option>
+                  <option value="cavatica">Execute in CAVATICA</option>
+                </select>
+              </div>
+            </div>
+            <div class="row px-4 px-lg-3 pb-4 justify-content-center">
+              <div class="col col-lg-9 px-lg-3 my-auto px-4 text-center alert alert-info">
+                <a href="/#/account/integrations">Click here</a> to configure additional integrations.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  {/if}
   <div class="row">
     <div class="col-sm-12">
       &nbsp;
