97b87bb8c437034be9c793eab59ff2a088d4c26b
Daniel Clarke <u8sand@gmail.com>
2022-03-29 11:13:40 -0400

    Adopt a simpler catalog-integration function registration mechanism

--- js/extras/catalog-integration/pagehit.js
+++ js/extras/catalog-integration/pagehit.js
@@ -2,11 +2,17 @@ import get_require from '@/utils/get_require'
 
 export default function pagehit(pagehit_type) {
   if (window._config.EXTRAS.indexOf('catalog-integration') !== -1) {
-    get_require(window, 'pagehit')
-      .then(catalog_pagehit => catalog_pagehit(pagehit_type))
-      .catch(e => {
-        console.error('catalog-integration: pagehit error')
-        console.error(e)
-      })
+    if (window._config.pagehit !== undefined) {
+      window._config.pagehit(pagehit_type)
+    } else {
+      console.info(`[catalog-integration:pagehit]: ${pagehit_type}`)
+      console.warn('[catalog-integration:pagehit]: attempting deprecated requirejs resolution')
+      get_require(window, 'pagehit')
+        .then(catalog_pagehit => {
+          console.warn('[catalog-integration:pagehit]: deprecated requirejs pagehit')
+          catalog_pagehit(pagehit_type)
+        })
+        .catch(e => {})
+    }
   }
 }
--- js/extras/catalog-integration/report_error.js
+++ js/extras/catalog-integration/report_error.js
@@ -3,13 +3,21 @@ import get_require from '@/utils/get_require'
 export default function report_error({ type, error }) {
   console.error(`[${type}]`, error)
   if (window._config.EXTRAS.indexOf('catalog-integration') !== -1) {
-    get_require(window, 'report_error')
-      .then(report_error => report_error({
-        appyter: ((nb || {}).metadata || {}).appyter || null,
-        url: window.location.href,
-        type,
-        error,
-      }))
-      .catch((e) => console.error('catalog-integration: failed to locate report_error handler'))
+    if (window._config.report_error !== undefined) {
+      window._config.report_error({ type, error })
+    } else {
+      console.warn('[catalog-integration:report_error]: attempting deprecated requirejs resolution')
+      get_require(window, 'report_error')
+        .then(report_error => {
+          console.warn('[catalog-integration:report_error]: deprecated report_error')
+          report_error({
+            appyter: ((nb || {}).metadata || {}).appyter || null,
+            url: window.location.href,
+            type,
+            error,
+          })
+        })
+        .catch((e) => {})
+    }
   }
 }
