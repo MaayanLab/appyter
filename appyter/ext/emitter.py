eaeea60d8ea98b8cfbb21be1ba0cd0fd90c79454
Daniel Clarke <u8sand@gmail.com>
2022-04-05 18:52:59 -0400

    Fallback to stderr emitter if socketio emitter fails

--- appyter/ext/emitter.py
+++ appyter/ext/emitter.py
@@ -1,4 +1,5 @@
 import contextlib
+import traceback
 import logging
 logger = logging.getLogger(__name__)
 
@@ -17,16 +18,45 @@ async def url_to_emitter(url):
   elif url.startswith('http://') or url.startswith('https://'):
     from appyter.ext.socketio import AsyncClient
     import urllib.parse
-    async with AsyncClient() as sio:
-      url_parsed = urllib.parse.urlparse(url)
-      url_path, room = url_parsed.path.rsplit('/', maxsplit=1)
-      await sio.connect(f"{url_parsed.scheme}://{url_parsed.netloc}", socketio_path=url_path)
-      await sio.emit('join', room)
-      async def emit(msg):
-        await sio.emit('forward', dict(event=msg['type'], data=msg['data'], to=room))
-      try:
-        yield emit
-      finally:
-        await sio.disconnect()
+    with fsspec.open('file:///dev/stderr', 'w') as fw:
+      fallback_emitter = json_emitter_factory(fw)
+      async with AsyncClient() as sio:
+        url_parsed = urllib.parse.urlparse(url)
+        url_path, room = url_parsed.path.rsplit('/', maxsplit=1)
+        try:
+          await sio.connect(f"{url_parsed.scheme}://{url_parsed.netloc}", socketio_path=url_path)
+        except:
+          await fallback_emitter(dict(
+            type='error',
+            data=f"Failed to connect to {url}... falling back to stderr",
+            traceback=traceback.format_exc(),
+          ))
+        else:
+          try:
+            await sio.emit('join', room)
+          except:
+            await fallback_emitter(dict(
+              type='error',
+              data=f"Failed to join {room}...",
+              traceback=traceback.format_exc(),
+            ))
+        async def emit(msg):
+          try:
+            await sio.emit('forward', dict(event=msg['type'], data=msg['data'], to=room))
+          except:
+            await fallback_emitter(dict(msg,
+              traceback=traceback.format_exc(),
+            ))
+        try:
+          yield emit
+        finally:
+          try:
+            await sio.disconnect()
+          except:
+            await fallback_emitter(dict(
+              type='error',
+              data=f"Failed to disconnect.",
+              traceback=traceback.format_exc(),
+            ))
   else:
     raise NotImplementedError
