16a0cbbd15458081dcec02dc9aaa707df9e25503
Daniel Clarke <u8sand@gmail.com>
2022-04-07 16:00:51 -0400

    WIP: Add FileField, now a TabField of other FileFields

---
+++ js/profiles/default/js/fields/FileField.svelte
@@ -0,0 +1,57 @@
+<script>
+  import TabField from '@/components/fields/TabField.svelte'
+  import Lazy from '@/components/Lazy.svelte'
+
+  export let args
+
+  let tabArgs = {
+    name: `${args.name}-type`,
+    label: args.label,
+    description: args.description,
+    value: 'Upload',
+    choices: [
+      'Upload',
+      window._config.EXTRAS.includes('catalog-integration') && 'Locate',
+      'Passthrough'
+    ].filter(choice => choice !== false),
+  }
+</script>
+
+<TabField bind:args={tabArgs} let:tab={tab}>
+  {#if tab === 'Upload'}
+    <Lazy
+      module={() => import('./UploadFileField.svelte')}
+      props={{
+        args: {
+          ...args,
+          name: `${args.name}-upload`,
+          label: 'Upload',
+        }
+      }}
+    />
+  {:else if window._config.EXTRAS.includes('catalog-integration') && tab === 'Locate'}
+    <Lazy
+      module={() => import('./StorageFileField.svelte')}
+      props={{
+        args: {
+          ...args,
+          name: `${args.name}-locate`,
+          label: 'Browse Your Uploads & Integrations',
+          storage: 'account://',
+        }
+      }}
+    />
+  {:else if tab === 'Passthrough'}
+    <Lazy
+      module={() => import('./URIFileField.svelte')}
+      props={{
+        args: {
+          name: `${args.name}-uri`,
+          label: 'Uniform Resource Identifier',
+          description: 'One of several supported identifiers for accessing your file',
+          examples: Object.keys(args.examples).reduce((agg, example) => (example.indexOf('://') !== -1) ? { ...agg, [example]: args.examples[example] } : agg, {}),
+        },
+      }}
+    />
+  {/if}
+</TabField>
