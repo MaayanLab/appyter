7381677939f214971fdd4f39e741675f61f082d4
Daniel Clarke <u8sand@gmail.com>
2022-04-19 14:51:52 -0400

    Add CHUNK_PADDING to ensure chunking is worth it/not re-done when forwarding

--- appyter/ext/socketio/chunked_emit.py
+++ appyter/ext/socketio/chunked_emit.py
@@ -7,10 +7,11 @@ class ChunkedEmitMixin:
   ''' A mixin for splitting up large `emit` calls into `chunked` calls of lower priority
   '''
   CHUNK_SIZE = 1<<20 # 1 kb
+  CHUNK_PADDING = 1<<8 # don't re-chunk forwarded chunks, size of padding around the chunk
 
   async def emit(self, evt, data, priority=0, **kwargs):
     serialized = json.dumps(data, sort_keys=True, separators=None, ensure_ascii=True)
-    if len(serialized) > self.CHUNK_SIZE:
+    if len(serialized) > self.CHUNK_SIZE + self.CHUNK_PADDING:
       logger.debug(f"Large packet ({len(serialized)}), chunking...")
       sha1 = hashlib.sha1()
       sha1.update(serialized.encode())
