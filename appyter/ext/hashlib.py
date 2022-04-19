f1ef68c160825eda20ba16b84c19561c14630a96
Daniel Clarke <u8sand@gmail.com>
2022-04-19 16:39:36 -0400

    Fix chunking with async serialization/hashing

--- appyter/ext/hashlib.py
+++ appyter/ext/hashlib.py
@@ -11,10 +11,16 @@ def hashsum_io(algo, io, chunk_size=65536):
 
 sha1sum_io = functools.partial(hashsum_io, 'sha1')
 
-def hashsum_dict(algo, obj):
-  import json, hashlib
+def hashsum_str(algo, obj):
+  import hashlib
   sha1 = hashlib.new(algo)
-  sha1.update(json.dumps(obj, sort_keys=True, separators=None, ensure_ascii=True).encode())
+  sha1.update(obj.encode())
   return sha1.hexdigest()
+
+sha1sum_str = functools.partial(hashsum_str, 'sha1')
+
+def hashsum_dict(algo, obj):
+  import json
+  return hashsum_str(algo, json.dumps(obj, sort_keys=True, separators=None, ensure_ascii=True))
 
 sha1sum_dict = functools.partial(hashsum_dict, 'sha1')
--- appyter/ext/socketio/chunked_emit.py
+++ appyter/ext/socketio/chunked_emit.py
@@ -1,28 +1,32 @@
 import json
-import hashlib
 import logging
+
+from appyter.ext.asyncio.helpers import ensure_async
+from appyter.ext.hashlib import hashsum_str
 logger = logging.getLogger(__name__)
 
 class ChunkedEmitMixin:
   ''' A mixin for splitting up large `emit` calls into `chunked` calls of lower priority
   '''
-  CHUNK_SIZE = 1<<20 # 1 kb
-  CHUNK_PADDING = 1<<8 # don't re-chunk forwarded chunks, size of padding around the chunk
+  CHUNK_SIZE = 1<<20 # 1kb
+  CHUNK_OVERHEAD = 1<<8 # not worth it if it doesn't exceed overhead
 
   async def emit(self, evt, data, priority=0, **kwargs):
-    serialized = json.dumps(data, sort_keys=True, separators=None, ensure_ascii=True)
-    if len(serialized) > self.CHUNK_SIZE + self.CHUNK_PADDING:
+    chunkable = evt not in {'chunked', 'forward'}
+    if chunkable:
+      serialized = await ensure_async(json.dumps)(data, sort_keys=True, separators=None, ensure_ascii=True)
+      chunkable = len(serialized) > (self.CHUNK_SIZE + self.CHUNK_OVERHEAD)
+    #
+    if chunkable:
       logger.debug(f"Large packet ({len(serialized)}), chunking...")
-      sha1 = hashlib.sha1()
-      sha1.update(serialized.encode())
-      data_hash = sha1.hexdigest()
-      n_chunks = len(serialized) // ChunkedEmitMixin.CHUNK_SIZE + 1
+      data_hash = await ensure_async(hashsum_str)(serialized)
+      n_chunks = (len(serialized) // ChunkedEmitMixin.CHUNK_SIZE) + int(len(serialized) % ChunkedEmitMixin.CHUNK_SIZE != 0)
       for n in range(0, n_chunks):
         await super().emit(
           'chunked',
           dict(
             id=data_hash, event=evt, chunk=n, total=n_chunks,
-            data=serialized[n*self.CHUNK_SIZE:n*self.CHUNK_SIZE+self.CHUNK_SIZE]
+            data=serialized[n*self.CHUNK_SIZE:(n+1)*self.CHUNK_SIZE]
           ),
           priority=priority+1,
           **kwargs,
