fe855c2b7efc5637573130d2bb035ad56d83e7bf
Daniel Clarke <u8sand@gmail.com>
2022-04-19 16:38:21 -0400

    Slight improvement to forward debug logging

--- appyter/ext/socketio/forwarding.py
+++ appyter/ext/socketio/forwarding.py
@@ -6,7 +6,7 @@ class ForwardingMixin:
   '''
   async def emit(self, evt, data, to=None, priority=0, **kwargs):
     if to is not None:
-      logger.debug('Message forwarding')
+      logger.debug(f"Message forwarding {evt} to {to}")
       return await super().emit('forward', dict(event=evt, data=data, to=to, priority=priority))
     else:
       return await super().emit(evt, data, to=to, priority=priority)
