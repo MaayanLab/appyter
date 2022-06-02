c741c3a3b69340ac4f00ed5bf65345a61550f8fc
Daniel Clarke <u8sand@gmail.com>
2022-06-02 16:23:18 -0400

    WIP: Use TextListField for multiple URI passthrough

--- js/extras/file-field/URI.svelte
+++ js/extras/file-field/URI.svelte
@@ -1,7 +1,7 @@
 <script>
-import StringField from '@/components/fields/StringField.svelte';
-
   import Lazy from '@/components/Lazy.svelte';
+  import StringField from '@/components/fields/StringField.svelte';
+  import TextListField from '@/components/fields/TextListField.svelte';
 
   export let args
   let stringArgs = {
@@ -22,9 +22,13 @@ import StringField from '@/components/fields/StringField.svelte';
   <span style="font-weight: 500">URI Passthrough</span>: This allows you to pass the file through a number of different URI protocols. Supported protocols include: drs (GA4GH), s3, gs, ftp, http, & https
 </Lazy>
 
-<StringField bind:args={stringArgs} />
+{#if args.multiple}
+  <TextListField bind:args={stringArgs} />
+{:else}
+  <StringField bind:args={stringArgs} />
+{/if}
 
-<input
+<textarea
   type="text"
   class="hidden"
   name={args.name}
