681b7f5ca1574ed9f2c308b8134ba9b0e5089e55
Daniel Clarke <u8sand@gmail.com>
2022-06-15 10:48:42 -0400

    Fix reversion in TextListField

--- js/profiles/biojupies/js/fields/TextListField.svelte
+++ js/profiles/biojupies/js/fields/TextListField.svelte
@@ -1,6 +1,14 @@
 <script>
   import TextListField from '@/components/fields/TextListField.svelte';
+  import ensure_string_list from '@/utils/ensure_string_list'
+
   export let args
 </script>
 
-<TextListField args={args} />
+<TextListField
+  args={{
+    ...args,
+    default: ensure_string_list(args.default).join('\n'),
+    value: ensure_string_list(args.value).join('\n')
+  }}
+/>
