72ed0225711cd360eea74fc1cf1a121b25bdcdfb
Daniel Clarke <u8sand@gmail.com>
2025-06-25 18:15:18 -0400

    Remove unneded extensions for doc building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,12 +14,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-import sphinx
-import commonmark
-from m2r2 import MdInclude
-from recommonmark.transform import AutoStructify
-from textwrap import dedent
-
 # -- Project information -----------------------------------------------------
 
 project = "Appyter"
@@ -37,14 +31,14 @@ release = '0.0.1'
 # ones.
 
 extensions = [
-  'recommonmark',
+  'sphinx_mdinclude',
   'sphinx.ext.autodoc',
   'sphinx.ext.viewcode',
   'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -62,42 +56,6 @@ html_theme = 'nature'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 autosectionlabel_prefix_document = True
-
-class CustomReStructuredTextRenderer(commonmark.ReStructuredTextRenderer):
-    def code_block(self, node, entering):
-        language_name = None
-
-        info_words = node.info.split() if node.info else []
-        if len(info_words) > 0 and len(info_words[0]) > 0:
-            language_name = info_words[0]
-
-        if language_name == 'eval_rst':
-            self.out(dedent(node.literal))
-        else:
-            super().code_block(node, entering)
-
-def docstring(app, what, name, obj, options, lines):
-    md  = '\n'.join(lines)
-    ast = commonmark.Parser().parse(md)
-    rst = CustomReStructuredTextRenderer().render(ast)
-    lines.clear()
-    lines += rst.splitlines()
-
-def setup(app):
-    config = {
-        # 'url_resolver': lambda url: github_doc_root + url,
-        'auto_toc_tree_section': 'Contents',
-        'enable_eval_rst': True,
-    }
-    app.add_config_value('recommonmark_config', config, True)
-    app.add_transform(AutoStructify)
-    app.connect('autodoc-process-docstring', docstring)
-    app.add_config_value('no_underscore_emphasis', False, 'env')
-    app.add_config_value('m2r_parse_relative_links', False, 'env')
-    app.add_config_value('m2r_anonymous_references', False, 'env')
-    app.add_config_value('m2r_disable_inline_math', False, 'env')
-    app.add_config_value('m2r_use_mermaid', False, 'env')
-    app.add_directive('mdinclude', MdInclude)
